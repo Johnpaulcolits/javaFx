8a5051833fc34e28cfa2cf117644fb96e4ea8a6a
John Paul <110954512+Johnpaulcolits@users.noreply.github.com>
2023-06-05 00:03:42 +0800

    Add files via upload

---
+++ Classis/src/classis/Main.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package classis;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author USER
+ */
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+    try{
+          
+    Parent root= FXMLLoader.load(getClass().getResource("Coffee.fxml"));
+     Scene scene=new Scene(root,700,500); 
+     scene.getStylesheets().add(getClass().getResource("coffee.css").toExternalForm());
+     primaryStage.setTitle("Welcome To KapeShop");
+     
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }catch(Exception e){
+        e.printStackTrace();
+    }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
